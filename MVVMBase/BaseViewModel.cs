7898dcadefe8b12d3314d12cb47e1c63c068fbf4
Konstantin <kospost@gmail.com>
2015-04-18 22:55:12 +0300

    Init

---
+++ MVVMBase/BaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace MVVMBase
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        protected void OnPropertyChanged<T>(Expression<Func<T>> action)
+        {
+            string propertyName = GetPropertyName(action);
+            this.OnPropertyChanged(propertyName);
+        }
+
+        private static string GetPropertyName<T>(Expression<Func<T>> action)
+        {
+            var expression = (MemberExpression)action.Body;
+            string propertyName = expression.Member.Name;
+            return propertyName;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+    }
+}
---
+++ MVVMBase/DelegateCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+
+namespace MVVMBase
+{
+    /// <summary>
+    /// Provides a simple ICommand implementation.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+
+        private readonly Predicate<object> canExecute;
+
+        /// <summary>
+        /// Initializes a new instance of the  class.
+        /// </summary>
+        /// <param name="execute">The execute action.</param>
+        public DelegateCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the  class.
+        /// </summary>
+        /// <param name="execute">The execute action.</param>
+        /// <param name="canExecute">The can execute predicate.</param>
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>
+        /// True if this command can be executed, otherwise - false.
+        /// </returns>
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
